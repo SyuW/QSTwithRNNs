16be4b1f7b81e5e586734b4a909e6ec4391d9e9f
SyuW <49294470+SyuW@users.noreply.github.com>
2021-11-11 17:45:49 -0500

    small RNN.py update

--- RNN.py
+++ RNN.py
@@ -8,5 +8,17 @@ import torch
 import numpy
 import argparse
 
+
+class ConventionalRNN:
+
+    def get_data(self):
+        return
+
+    def backprop(self):
+        return
+
+    def __init__(self):
+        return
+
 if __name__ == "__main__":
     pass
